45ea25cfe0a859889e797b6f7ee1e0564052a153
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-11-03 11:39:47 +0530

    functions types based on args and return values

---
+++ days/251103/discussion.py
@@ -0,0 +1,19 @@
+1. multi module
+    employee_model.py
+    ->
+    /app 
+        - employee.py       Employee
+        - repo.py           crud fns, employees 
+        - run.py            imported employee and repo modules 
+
+2. functions 
+    takes args
+    may returns result : one or more 
+
+    args 
+    - positional arg 
+    - keyword arg 
+    - default arg 
+    - variable arg 
+        - positional variable args :: tuple  
+        - keyword variable args :: dict 
