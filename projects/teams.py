a4f47de2afd578570d11da6e61e4a53f4489aec0
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-11-05 16:17:32 +0530

    team formation

---
+++ projects/teams.py
@@ -0,0 +1,11 @@
+Product PMS : 
+    - lavanya, iram
+Bank BMS:
+    - abhishek ojha, nihar, rishabh
+    - pratik dhumal, pratik sidnale, jahnavi
+    - abhishek kashyap, bibhishan, sujal
+Patient HMS:
+    - divyashree, lasya, hamsika
+    - gautam, harish, tarun
+    - masood, aarish
+
